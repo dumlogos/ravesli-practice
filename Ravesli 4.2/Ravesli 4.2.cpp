478a53902e1bb3be1f7281443d82befe3a6c3719
DumLogos <andkudry@gmail.com>
2020-06-20 18:33:32 +0300

    Ravesli 4.2. task done

---
+++ Ravesli 4.2/Ravesli 4.2.cpp
@@ -0,0 +1,39 @@
+﻿/*Написать программу, которая вычисляет среднее арифметическое 
+последовательности дробных чисел, вводимых с клавиатуры. 
+После ввода последнего числа программа должна вывести минимальное 
+и максимальное числа из последовательности. 
+Количество чисел последовательности вводит пользователь.*/
+
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int cnt;
+	float min, max;
+	float averrage = 0;
+
+	std::cout << "Enter the count of numbers: ";
+	std::cin >> cnt;
+
+	float bufNum;
+	for (int i = 0; i < cnt; ++i) {
+		std::cin >> bufNum;
+		if (i == 0) {
+			min = bufNum;
+			max = bufNum;
+		}
+		else if (bufNum > max) max = bufNum;
+		else if (bufNum < min) min = bufNum;
+		averrage += bufNum;
+	}
+
+	std::cout << "Number average equals " << averrage / cnt << std::endl;
+	std::cout << "Max equals " << max << std::endl;
+	std::cout << "Min equals " << min << std::endl;
+
+
+	return 0;
+}
+
